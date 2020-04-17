033322f77afb9009621ac26ce2ba7cb9781a8852
pedrost <pee.dro_fs@hotmail.com>
2020-04-17 16:20:25 -0400

    added one more instance methohod

--- live/lib/customer.rb
+++ live/lib/customer.rb
@@ -1,8 +1,13 @@
 class Customer
-    attr_accessor :security_code
+    attr_accessor :security_code, :score
 
     def initialize(params = {})
-        @security_code = params.fetch(:security_code)
+        @security_code = params.fetch(:security_code, nil)
+        @score = 0
+    end
+
+    def buy_items(quantity)
+    	@score += quantity
     end
 
 end
--- live/spec/models/customer_spec.rb
+++ live/spec/models/customer_spec.rb
@@ -5,28 +5,42 @@ RSpec.describe Customer do
   describe '#security_code?' do
 
     context 'when code is valid' do
-      //Arrange
+      # Arrange
       let(:security_code) { '1020secret' }
       subject(:customer) { described_class.new({ security_code: security_code })}
 
-      //Act
+      # Act
       it 'should return true' do
-        //Asser
+        # Assert
         expect(customer.security_code.end_with?('secret')).to eq(true)
       end
     end
 
     context 'when code is invalid' do
-      //Arrange
+      # Arrange
       let(:security_code) { '1020segredo' }
       subject(:customer) { described_class.new({ security_code: security_code })}
-      //Act
+      # Act
       it 'should return false' do
-        //Assert
+        # Assert
         expect(expect(customer.security_code.end_with?('secret')).to eq(false))
       end
     end
+  end
 
+  describe '#score' do
+    context 'when customer bought an item', focus: true do
+      # Arrange
+      subject(:customer) { described_class.new({}) }
+
+      # Act
+      it 'sum one to the socre for each item he bought' do
+        10.times { customer.buy_items(2) }
+
+        # Assert
+        expect(customer.score).to eq(20)
+      end
+    end
   end
 
 end
